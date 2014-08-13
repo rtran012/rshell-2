4cf3927cd2b048dcb14cc56372fcf05c478402d4
kle016 <kle016@ucr.edu>
2014-08-13 16:14:39 -0700

    Update ls.cpp

--- src/ls.cpp
+++ src/ls.cpp
@@ -42,60 +42,68 @@ cout<<'-';
 
 }
 
-void print_file( char* path, struct stat& buf ) {
-permissions(buf);
+void print_file( char* path, struct stat& buf ) 
+{
+	
+	permissions(buf);
+	time_t now = buf.st_mtime;
+	tm * ptm = localtime(&now);
+	char buffer[30];
+	strftime(buffer, 30, "%b %d %H:%M:%S", ptm); 
 
-time_t now = buf.st_mtime;
-std::tm * ptm = std::localtime(&now);
-char buffer[30];
-strftime(buffer, 30, "%b %d %H:%M:%S", ptm); 
-
-cout << '\t' << buf.st_nlink << '\t' 
-<< buf.st_size << '\t'
-<< buffer << '\t'
-<< path << endl; 
+	cout << '\t' << buf.st_nlink << '\t' 
+	<< buf.st_size << '\t'
+	<< buffer << '\t'
+	<< path << endl; 
 }
 
-void print_directory( char* path, int width ) {
+void print_directory( char* path, int width ) 
+{
 
-unsigned char isFile = 8;
-unsigned char isDir = 4;
-DIR* dir;
-struct dirent* entry;
-
-if ( (dir = opendir (path)) != NULL) {
-
-// print all the files and directories within directory
-while ((entry = readdir (dir)) != NULL) {
-
-if( (entry -> d_name)[0] == '.') {
-continue;
+	unsigned char isFile = 8;
+	unsigned char isDir = 4;
+	DIR* dir;
+	struct dirent* entry;
+	
+	if ( (dir = opendir (path)) != NULL) {
+	
+	// print all the files and directories within directory
+	while ((entry = readdir (dir)) != NULL) {
+	
+	if( (entry -> d_name)[0] == '.') {
+	continue;
 }
 
-for( int i = 0; i < width; i++ ) {
-cout << "\t";
+for( int i = 0; i < width; i++ ) 
+{
+	cout << "\t";
 }
 
-if( entry -> d_type == isFile ) {
-cout << entry -> d_name << endl;	
-} else {
-cout << endl;
-cout << "./" << entry -> d_name << ":" << endl;
+if( entry -> d_type == isFile ) 
+{
+	cout << entry -> d_name << endl;	
+} 
+else 
+{
+	cout << endl;
+	cout << "./" << entry -> d_name << ":" << endl;
 }
-if( entry -> d_type == isDir ) {
-strcat(path, "/");
-strcat(path, entry -> d_name);
-print_directory( path, width + 1 );
+if( entry -> d_type == isDir ) 
+{
+	strcat(path, "/");
+	strcat(path, entry -> d_name);
+	print_directory( path, width + 1 );
 }
 
 }
 
 closedir (dir);
 
-} else {
-//could not open directory 
-cout << "Could not open directory." << endl;
-return;
+} else 
+{
+	//could not open directory 
+	cout << "Could not open directory." << endl;
+	return;
 }
 }
 
@@ -153,20 +161,29 @@ char *path = argv[1];
 struct stat buf;
 cin.ignore();
 
-if ( stat(path, &buf) != -1 ) {
-if( S_ISREG(buf.st_mode) ) {
-print_file( path, buf );
-} else if( S_ISDIR(buf.st_mode) ) {
-print_directory( path, 0 );
-}
-} else {
-cerr << "Error " << errno << " opening " << path << endl;
-}
-} else {
-
-string tmp = ".";
-print_default_dir(tmp,0);
-}
+if ( stat(path, &buf) != -1 ) 
+{
+	if( S_ISREG(buf.st_mode) ) 
+	{
+		print_file( path, buf );
+	} 
+	else if( S_ISDIR(buf.st_mode) ) 
+	{
+		print_directory( path, 0 );
+	}
+	} 
+	else 
+	{
+		cerr << "Error " << errno << " opening " << path << endl;
+	}
+	
+	} 
+	else 
+	{
+	
+		string tmp = ".";
+		print_default_dir(tmp,0);
+	}
 
 return 0;
 }
